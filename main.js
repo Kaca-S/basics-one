42b037d7ee7577d5d4ba4af5706e6e5781d5aab1
Kaca-S <karolinasosic@gmail.com>
2025-03-18 13:53:36 +0100

    created 2 variables with numbers and performed basic arithmetic operations

--- main.js
+++ main.js
@@ -6,7 +6,22 @@ console.log("Hello World");
 
 let firstName = "Karolina";
 let age = 38;
-let = "blue";
+let favouriteColor = "blue";
 
-console.log(typeof firstName);
-console.log(`My age is ${age}`);
+// console.log(typeof firstName);
+// console.log(`My age is ${age}`);
+
+console.log(`My name is ${firstName}, I am ${age} and my color is ${favouriteColor}`);
+
+//Exercise 3
+
+let oranges = 50;
+let lemons = 10;
+
+console.log(oranges);
+console.log(lemons);
+
+console.log(`Addition ${oranges + lemons}`);
+console.log(`Subtraction ${oranges - lemons}`);
+console.log(`Multiplication ${oranges * lemons}`);
+console.log(`Division ${oranges / lemons}`);
