d61dc57e526394345e3f8053feccdbe297af67a9
Kaca-S <karolinasosic@gmail.com>
2025-03-18 13:06:19 +0100

    used console.log() function

---
+++ main.js
@@ -0,0 +1,12 @@
+//Exercise 1
+
+console.log("Hello World");
+
+//Exercise 2
+
+let firstName = "Karolina";
+let age = 38;
+let = "blue";
+
+console.log(typeof firstName);
+console.log(`My age is ${age}`);
